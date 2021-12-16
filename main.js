62fad0113efdd3c7e80a04d202e0c9d2bd0f5065
Jyotir Sai <jyotir@Jyotirs-MacBook-Pro.local>
2021-12-15 22:05:07 -0700

    attempting bubble sort

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", randomizeArray(), false);
+
 function randomizeArray() {
   const arrays = document.getElementById("arrays");
   arrays.innerHTML = "";
@@ -10,5 +12,22 @@ function randomizeArray() {
     const height = Math.floor(Math.random() * 500);
     rectangle.style.height = height + "px";
     arrays.appendChild(rectangle);
+  }
+}
+
+function bubbleSort() {
+  const arrays = document.getElementById("arrays");
+  const children = arrays.children;
+  for (let i = 0; i < children.length - 1; i++) {
+    height1 = Number(children[i].style.height.match(/(\d+)/)[0]);
+    height2 = Number(children[i + 1].style.height.match(/(\d+)/)[0]);
+
+    if (height1 > height2) {
+      children[i].style.height = String(height2) + "px";
+      children[i + 1].style.height = String(height1) + "px";
+    }
+  }
+  for (let i = 0; i < children.length; i++) {
+    console.log(children[i].style.height);
   }
 }
