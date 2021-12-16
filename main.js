fe167ca156aca6a1112d5901f7765537de24b96e
Jyotir Sai <jyotir@Jyotirs-MacBook-Pro.local>
2021-12-16 13:00:30 -0700

    finished bubble sort

--- main.js
+++ main.js
@@ -15,11 +15,13 @@ function randomizeArray() {
   }
 }
 
-function bubbleSort() {
+async function bubbleSort() {
   const arrays = document.getElementById("arrays");
   const children = arrays.children;
   for (let i = 0; i < children.length - 1; i++) {
     for (let j = 0; j < children.length - i - 1; j++) {
+      children[j].style.background = "red";
+      children[j + 1].style.background = "red";
       height1 = Number(children[j].style.height.match(/(\d+)/)[0]);
       height2 = Number(children[j + 1].style.height.match(/(\d+)/)[0]);
 
@@ -27,6 +29,17 @@ function bubbleSort() {
         children[j].style.height = String(height2) + "px";
         children[j + 1].style.height = String(height1) + "px";
       }
+
+      await new Promise((resolve) => setTimeout(resolve, 1));
+
+      children[j].style.background = "black";
+      children[j + 1].style.background = "black";
     }
   }
+}
+
+async function insertionSort() {
+  const arrays = document.getElementById("arrays");
+  const children = arrays.children;
+  for (let i = 0; i < children.length; i++) {}
 }
